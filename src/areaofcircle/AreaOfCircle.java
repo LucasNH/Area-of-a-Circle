aecd63fc85f74986216c928fb2b61141a2f819a5
lunor7649 <lunor7649@7HJL6Y1.Student.UGDSB.ED>
2018-02-28 11:37:50 -0500

    Commit for AreaOfCircle

---
+++ src/areaofcircle/AreaOfCircle.java
@@ -0,0 +1,21 @@
+/*
+AreaOfCircle.java 
+Find the area of a circle with a radius of 15 cm. Display the answer in
+sentence form.
+Created by: Lucas N-H
+Created on: February 26, 2018
+Last edited on: February 28, 2018
+*/
+package areaofcircle;
+
+public class AreaOfCircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double area;
+        area = 3.141592653589793 *(15*15);
+        System.out.println("The area of the circle is "+area+"cm^2.");
+    }
+}
